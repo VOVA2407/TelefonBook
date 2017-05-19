95386b3497607213639409acc49bb0b08e36093a
VOVA2407 <Admin@YAKOVLEV-VI-NOTE.active.local>
2017-05-19 13:38:42 +0300

    vova

--- TelefonBook/Properties/AssemblyInfo.cs
+++ TelefonBook/Properties/AssemblyInfo.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices;
 #endregion
 // просто коммент для проверки
 //второй коммент для проверки
+//третий коммент
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
