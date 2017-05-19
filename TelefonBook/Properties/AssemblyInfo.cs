d881595d0c12271eeb2c97dd8d2c43b23220811e
VOVA2407 <fanrammstein@bk.ru>
2017-05-19 13:48:11 +0300

    vova5

--- TelefonBook/Properties/AssemblyInfo.cs
+++ TelefonBook/Properties/AssemblyInfo.cs
@@ -9,6 +9,7 @@ using System.Runtime.InteropServices;
 //второй коммент для проверки
 //третий коммент
 //четвертый коммент
+//пятый коммент
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
