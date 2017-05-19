eb4b0598a11e0cd267bf86e30e0629d87326e6e0
VOVA2407 <Admin@YAKOVLEV-VI-NOTE.active.local>
2017-05-19 13:13:27 +0300

    Init

---
+++ TelefonBook/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Admin
+ * Date: 16.05.2017
+ * Time: 16:15
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TelefonBook
+{
+	public class phoneBook
+	{
+		string name;
+		public string Name { get; set; }
+		public string Surname { get; set;}
+		public string Adress { get; set;}
+		
+		public string Name1
+		{
+			get {return name;}
+			set {name = value;}
+		}
+		
+		public phoneBook()
+		{
+			name = "Нет данных";
+		}
+	}
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			phoneBook Book = new phoneBook();
+			// TODO: Implement Functionality Here
+			Book.Name1 = "Телефонная книга";
+			Book.Name = "Vladimir";
+			Book.Surname = "Iakovlev";
+			Book.Adress = "Sportivnaya";
+			Console.WriteLine("{0} {1} {2}", Book.Name, Book.Surname, Book.Adress);
+			Console.WriteLine("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
